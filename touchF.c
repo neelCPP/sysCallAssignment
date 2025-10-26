66326a1ccb1886e6099d5acb5ce136395bbc68d6
neelCPP <f20250896@goa.bits-pilani.ac.in>
2025-10-26 22:15:44 +0530

    Update touchF.c

--- touchF.c
+++ touchF.c
@@ -9,3 +9,18 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
+
+
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+    if(argc < 2 || argv[1] == "--c") printf("invalid format for more info use touch.c --help");
+    else{
+        if(argv[1] == "--help") printf("This helps you to make new file use it in format ./touch.exe <path_to_file>");
+        else{FILE *fp = fopen(argv[1],"a");
+        printf("Done the file %s is created %s",argv[1]);
+        }
+    }
+    return 0;
+}
+
