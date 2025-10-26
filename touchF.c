04632a875fdeeccecf5055407a6787421467adc4
neelCPP <f20250896@goa.bits-pilani.ac.in>
2025-10-26 17:15:36 +0000

    Update touchF.c

--- touchF.c
+++ touchF.c
@@ -1,26 +1,14 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(int argc, char *argv[]){
-    if(argc < 2) printf("The format is ./cat.c <name_of_file> please input the argument");
+    char s[10] = "--help";
+    if(argc < 2) printf("invalid format for more info use touch.c --help");
     else{
-        FILE *file = fopen(argv[1],"a");
-        printf("Done the file %s is created %s",argv[1]);
-    }
-    return 0;
-}
-
-
-
-#include<stdio.h>
-
-int main(int argc, char *argv[]){
-    if(argc < 2 || argv[1] == "--c") printf("invalid format for more info use touch.c --help");
-    else{
-        if(argv[1] == "--help") printf("This helps you to make new file use it in format ./touch.exe <path_to_file>");
+        if(!(strcmp(argv[1],s))) printf("This helps you to make new file use it in format ./touch.exe <path_to_file>");
         else{FILE *fp = fopen(argv[1],"a");
-        printf("Done the file %s is created %s",argv[1]);
+        printf("Done the file %s is created",argv[1]);
         }
     }
     return 0;
 }
-
